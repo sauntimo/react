d6a267b573f5e326bd1290b2d3ff646469165a5d
Tim Saunders <tim@sauntimo.org>
2018-08-05 13:32:09 +0100

    Hello World demo

---
+++ helloworld/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello World</h1>;
+
+const rootElem = document.getElementById( 'root' );
+
+ReactDOM.render( element, rootElem );
